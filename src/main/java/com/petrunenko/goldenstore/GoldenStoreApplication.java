a9a02e2f22c1125ed7acbed29b2665a1ec936289
Mikhail Petrunenko <Mikhail.Petrunenko@ipsotek.com>
2023-04-09 09:02:55 +0200

    init

---
+++ src/main/java/com/petrunenko/goldenstore/GoldenStoreApplication.java
@@ -0,0 +1,13 @@
+package com.petrunenko.goldenstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petrunenko/goldenstore/GoldenStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petrunenko.goldenstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
